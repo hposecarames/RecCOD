d4e3efc5a06d473b6ae5feaf398d195248ac75e3
menuven <hposecarames.Hector Pose Carames>
2018-12-18 09:35:56 +0100

    Renombre 2

--- factorial/Factorial.java
+++ factorial/Factorial.java
@@ -5,21 +5,21 @@ public class Factorial {
     public static void main(String[] args) {
 
         int j;
-        int f;
+        int factorizar;
 
         j = 8;
 
         int i;
         if (j == 0) {
-            f = 1;
+            factorizar = 1;
         } else {
-            f = 1;
+            factorizar = 1;
             for (i = j; i >= 1; i--) {
-                f = f * i;
+                factorizar = factorizar * i;
             }
         }
 
-        System.out.println(f);
+        System.out.println(factorizar);
 
     }
 
