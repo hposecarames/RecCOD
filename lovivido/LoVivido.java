27d0d9d877cfed1a236ec380f97c73811eaaa527
menuven <hposecarames.Hector Pose Carames>
2018-12-18 09:17:43 +0100

    Refactorizar y renombrar

---
+++ lovivido/LoVivido.java
@@ -0,0 +1,39 @@
+/*
+ * Examen Refactorizacion
+ * 
+ */
+package lovivido;
+
+import javax.swing.JOptionPane;
+
+public class LoVivido {
+
+    public static void main(String[] args) {
+        String nombre;
+        String edad;
+
+        int meses;
+        int dias;
+        int horas;
+        int años;
+
+        nombre = JOptionPane.showInputDialog("Escriba su nombre: ");
+        edad = JOptionPane.showInputDialog("Escriba su edad: ");
+
+        años = Integer.parseInt(edad);
+
+        meses = (años * 12);
+        dias = (años * 365);
+        horas = (dias * 24);
+
+        JOptionPane.showMessageDialog(null, "Meses: " + meses, "Numero de meses vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Días: " + dias, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Horas: " + horas, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+
+    }
+
+}
